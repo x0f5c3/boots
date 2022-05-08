b309509eb968d7d47162917bd01e9a4d441cd3fc
x0f5c3 <x0f5c3@tutanota.com>
2022-05-08 02:57:55 +0200

    Another one

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "boots",
 	Short:   "This will bootstrap your os",
-	Version: "v0.0.2", // <---VERSION---> Updating this version, will also create a new GitHub release.
+	Version: "v0.0.3", // <---VERSION---> Updating this version, will also create a new GitHub release.
 	// Uncomment the following lines if your bare application has an action associated with it:
 	// RunE: func(cmd *cobra.Command, args []string) error {
 	// 	// Your code here
